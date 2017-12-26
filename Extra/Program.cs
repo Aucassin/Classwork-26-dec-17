42d5869ea0f39e6b0e94a544e9ed52a226f9a352
student <levjohn1999@gmail.com>
2017-12-26 20:37:27 +0200

    2

---
+++ Extra/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra
+{
+    static class A
+    {
+        public static int a = 15;
+        public static void Move(int x, int y)
+        {
+            Console.WriteLine(x*y);
+        }
+    }
+    class B
+    {
+        public static int a;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A.Move(15, 10);
+            Console.WriteLine(A.a);
+            B b = new Extra.B();
+            B.a = 100;
+            Console.WriteLine(B.a);
+            B.a = 101;
+            Console.WriteLine(B.a);
+
+        }
+    }
+}
